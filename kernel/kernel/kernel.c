e049f6e680d928261c3c8a5ce06797af8f8c8734
= <=>
2024-07-18 18:09:22 +0100

    update (misc.asm, kernel.c, mem/mem.h): adiciona a paginação de memória

--- kernel/kernel/kernel.c
+++ kernel/kernel/kernel.c
@@ -4,9 +4,17 @@
 #include <stdbool.h>
 #include "../arch/i386/drivers/keyboard/keyboard.h"
 #include "../arch/i386/drivers/keyboard/statuscode.h"
+#include "../mem/mem.h"
+
+extern void enable_paging();
+extern void load_page_directory(uint32_t* page_directory);
 
 void kernel_main(void) {
     char pressed_char = '\0';
+    init_page_directory();
+    create_first_page_table();
+    load_page_directory(page_directory);
+    enable_paging();
     terminal_initialize();
     const int keyboard_init_status = keyboard_initialize();
     if(keyboard_init_status == OK || keyboard_init_status == DISABLE_CAPSLOCK_FAILED)
