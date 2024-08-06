1274e09e80af4b454cb4e0df25b26e9f1bdc3ad9
= <=>
2024-08-06 18:19:25 +0100

    update (stdio, string): Adiciona a opção de imprimir números inteiros com printf(...)

--- libc/string/inttostr.c
+++ libc/string/inttostr.c
@@ -4,36 +4,29 @@ void inttostr(int num, char *str) {
     int i = 0;
     int isNegative = 0;
 
-    // Handle 0 explicitly, otherwise empty string is printed for 0
     if (num == 0) {
         str[i++] = '0';
         str[i] = '\0';
         return;
     }
 
-    // In standard itoa(), negative numbers are handled only with 
-    // base 10. Otherwise numbers are considered unsigned.
     if (num < 0 && num != -2147483648) {
         isNegative = 1;
         num = -num;
     }
 
-    // Process individual digits
     while (num != 0) {
         int rem = num % 10;
         str[i++] = rem + '0';
         num = num / 10;
     }
 
-    // If the number is negative, append '-'
     if (isNegative) {
         str[i++] = '-';
     }
 
-    // Append string terminator
     str[i] = '\0';
 
-    // Reverse the string
     int start = 0;
     int end = i - 1;
     while (start < end) {
